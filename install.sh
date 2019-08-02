e9577a8d51760bcb31e1284a237b33f07b830c42
Eloi Gabaldon Ponsa <eloi.gabaldon@lacasaperlateulada.com>
2019-08-02 11:51:20 +0200

    updated installer

--- install.sh
+++ install.sh
@@ -1,12 +1,13 @@
 #! /bin/bash
 
 rm tasks.py
-
+pw=`pwd`
+base=`basename $pw`
 for m in `ls -d */ |grep ^[^_]`
 do
   cd $m
   . install.sh
   cd ..
 
- echo "from ${m%?}.launcher import $task_name" >> tasks.py
+ echo "from ${base%?}.${m%?}.launcher import $task_name" >> tasks.py
 done
