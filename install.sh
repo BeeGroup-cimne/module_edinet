4b9fc522067770522da68cb0f808e9b61b2fb15c
Eloi Gabaldon Ponsa <eloi.gabaldon@lacasaperlateulada.com>
2019-11-27 09:48:35 +0100

    add base_package

--- install.sh
+++ install.sh
@@ -1,11 +1,13 @@
 #! /bin/bash
 
 rm tasks.py
+c_pwd=`pwd`
+base=`basename $cpwd`
 for m in `ls -d */ |grep ^[^_]`
 do
   cd $m
   . install.sh
   cd ..
 
- echo "from ${m%?}.launcher import $task_name" >> tasks.py
+ echo "from $base.${m%?}.launcher import $task_name" >> tasks.py
 done
