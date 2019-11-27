0a0790abe43fed5749cffc3ea1c3e0ac40a5b23f
Eloi Gabaldon Ponsa <eloi.gabaldon@lacasaperlateulada.com>
2019-11-27 10:27:30 +0100

    add django as encoder

--- .modules_config_files/launcher.py
+++ .modules_config_files/launcher.py
@@ -2,6 +2,7 @@ from celery_backend import app
 import os
 import subprocess
 import json
+from django.core.serializers.json import DjangoJSONEncoder
 
 task_exec_file = "{{task_exec_file}}"
 task_name = "{{task_name}}"
@@ -12,5 +13,5 @@ def {{task_name}}(params):
     path = os.path.dirname(__file__)
     venv = "{}/venv/bin/python".format(path)
     file_exec = "{}/{}".format(path, task_exec_file)
-    x = subprocess.call([venv, file_exec, json.dumps(params)])
+    x = subprocess.call([venv, file_exec, json.dumps(params, cls=DjangoJSONEncoder)])
 
