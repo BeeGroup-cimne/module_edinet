09d7ceae1f9ca60ad52d232f2aa09cbe4088520c
Eloi Gabaldon Ponsa <eloi.gabaldon@lacasaperlateulada.com>
2021-05-03 17:49:08 +0200

    test

--- edinet_clean_hourly_data_etl/hadoop_metering_job.py
+++ edinet_clean_hourly_data_etl/hadoop_metering_job.py
@@ -78,7 +78,7 @@ class MRJob_clean_metering_data(MRJob):
             value['accumulated'] = float(ret[3])
         except:
             value['accumulated'] = np.NaN
-        yield "AAA", 12 #key, value
+        yield key, 12 #key, value
 
     # def reducer(self, key, values):
     #     """
